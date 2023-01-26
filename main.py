317b7798e662382271c6a1b553fadc084f0d9085
Bhavesh124 <chavanbhavesh60@gmail.com>
2023-01-26 14:44:10 +0530

    updated

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from Insurance.logger import logging
 from Insurance.exception import InsuranceException
 import os,sys
+from Insurance.utils import get_collection_as_dataframe
+from Insurance.entity.config_entity import DataIngestionConfig
+from Insurance.entity import config_entity
+from Insurance.components.dataingestion import DataIngestion
 
-def test_logger_and_exception():
+'''def test_logger_and_exception():
      try:
          logging.info("starting the test logger and exception")
          result = 3 / 0
@@ -11,10 +15,19 @@ def test_logger_and_exception():
 
      except Exception as e:
           logging.debug(str(e))
-          raise InsuranceException(e,sys)
+          raise InsuranceException(e,sys)'''
           
 if __name__ == '__main__':
      try:
-          test_logger_and_exception()
+          #test_logger_and_exception()
+          #get_collection_as_dataframe(database_name="INSURANCE",collection_name="INSURANCE_PROJECT")
+          training_pipeline_config = config_entity.TrainingPipelineConfig()
+
+          #data ingestion
+          data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config = training_pipeline_config)
+          print(data_ingestion_config.to_dict())
+          data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
+          data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+
      except Exception as e:
           print(e)
--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from typing import List
 requirement_file_name = 'requirements.txt'
 REMOVE_PACKAGE = '-e .'
 
-def get_requirement()->List[str]:
+def get_requirements()->List[str]:
     with open(requirement_file_name) as requirement_file:
-        requirement_list = requirement_file.readline()
-    requirement_list = [requirement_file.replace("/n","") for requirement_name in requirement_list]
+        requirement_list = requirement_file.readlines()
+    requirement_list = [requirement_name.replace("\n","") for requirement_name in requirement_list]
 
     if REMOVE_PACKAGE in requirement_list:
         requirement_list.remove(REMOVE_PACKAGE)
@@ -20,4 +20,5 @@ setup(name = "Insurance",
       author = "Bhavesh Chavan",
       author_email = "chavanbhavesh60@gmail.com",
       packages = find_packages(),
-      install_requires = get_requirement())
+      install_requires = get_requirements()
+      )
